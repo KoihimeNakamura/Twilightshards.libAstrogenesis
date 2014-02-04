f9c47fcf7be9830772860cf75bd2be3f57ff4a61
Koihime Nakamura <koihimenakamura@gmail.com>
2014-02-04 00:53:15 -0800

    - Has a Galaxy object for deserilization

---
+++ Galaxy.cs
@@ -0,0 +1,23 @@
+﻿namespace TwilightShards.libAstrogenesis
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Newtonsoft.Json;
+    
+    public class Galaxy
+    {
+        public string galName { get; set; }
+        public double innerFormationRadius { get; set; }
+        public double barOuterEdge { get; set; }
+        public double outerFormationRadius { get; set; }
+        public double maxStellarDensity { get; set; }
+
+
+        public override string ToString()
+        {
+            return galName + " Galaxy";
+        }
+    }
+}
